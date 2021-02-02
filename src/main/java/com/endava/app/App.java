80790f8509064b03ec19bf9e1decaddf79748b58
Viktorija Velinova <Viktorija.Velinova@endava.com>
2021-02-02 15:19:33 +0100

    Initial commit

---
+++ src/main/java/com/endava/app/App.java
@@ -0,0 +1,13 @@
+package com.endava.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
